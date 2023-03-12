c8d01c70299019cee67a0df4875c8d1c42022dfa
Denys Vitali <denys@denv.it>
2023-03-12 18:26:14 +0100

    ci: improve rendering tool

--- render/index.js
+++ render/index.js
@@ -4,10 +4,12 @@ console.log("Starting...");
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: process.env.PUPPETEER_ARGS.split(" ") || [],
+  });
   const page = await browser.newPage();
 
-  await page.goto('http://172.18.0.1:3001');
+  await page.goto(process.env.TARGET_URL || "http://172.18.0.1:3001");
   
   let margin = "1cm";
 
